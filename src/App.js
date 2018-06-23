42ae8b0041b4cbede41310e88634abdd3fe4bae6
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2018-06-22 21:33:51 -0700

    trying out a beforeinstallprompt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
 import Toggle from "./Toggle";
@@ -6,12 +6,15 @@ import Toggle from "./Toggle";
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="app-header">Welcome to the Toggle Me App</div>
-        <div className="container">
-          <Toggle />
+      <Fragment>
+        <button className="my-prompt">Add to Home Screen</button>
+        <div className="app">
+          <div className="app-header">Welcome to the Toggle Me App</div>
+          <div className="container">
+            <Toggle />
+          </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
