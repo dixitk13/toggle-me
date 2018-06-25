27023fdd4c7b47709229126603d841147c3a9b9e
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2018-06-24 18:11:28 -0700

    adding server js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const os = require("os");
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+const port = process.env.PORT || "8080";
+app.set("port", port);
+
+const hostname = os.hostname();
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on ${hostname} @ port ${port}`));
